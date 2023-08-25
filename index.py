351dd0caeff741dde169eb79b7f4d4ec384bacd9
MarcosDiM <marcosdimecom@gmail.com>
2023-08-24 21:42:57 -0300

    TP 1 agregado en word y TP 1 Parte 2 terminado

--- index.py
+++ index.py
@@ -1,36 +1,3 @@
-#DEFINICION DE VARIABLES 
-numero1 = 8
-numero2 = 5.5
-suma = numero1 + numero2
-resta = numero1 - numero2
-mult = numero1 * numero2
-div = numero1/numero2
-
-nombre = "Marcos"
-apellido = "Di Meco"
-precio = 25.2
-descuento = 0.3
-precio_fin= precio * descuento
-cadena = "El que no arriesga, no gana"
-longitud = len(cadena)
-
-precio1 = int(33.84) 
-nombre_completo = nombre +" "+ apellido
-
-edad = 21 -5 + 10
-altura = (1.75 * 4)/3
-nombre_mayusculas = nombre_completo.upper()
-nombre_minusculas = nombre_completo.lower()
-
-nombre_may_min = nombre_mayusculas.title()
-
-
-#IMPRESION DE VARIABLES
-print("Suma: ", suma)
-print("Resta: ", resta)
-print("Mutiplicacion: ", mult)
-print("Division: ", div)
-
 #PARTE 1.2 
 
 #1.	Calcular el perímetro y área de un rectángulo dada su base y su altura.
