fb43f4d2f1afcbafdc849bc2c2f50d5130e4795a
MarcosDiM <marcosdimecom@gmail.com>
2023-10-12 08:38:24 -0300

    Ejercicio variables dimensionadas

---
+++ ejercicios_extra/ejer_variables_dimensionadas/funcciones_var_dimensionadas.py
@@ -0,0 +1,11 @@
+
+
+#Ejercicio 2
+
+def users_adress(users):
+
+    adress_users = []
+    for i in range(len(users)):
+        adress_users.append(adress = users[i][3])
+    
+    print(adress_users)
---
+++ ejercicios_extra/ejer_variables_dimensionadas/variables_dimensionadas.py
@@ -0,0 +1,8 @@
+import funcciones_var_dimensionadas
+
+#Ejercicio 2 
+
+client = [{"Marcos Di Meco", 5, 3589.98, "Francia 430"}, {"Lautaro Tejada", 18, 7854.25, "San Martin 1898"}, {"Martin Ciccala", 29, 1736.86, "Las Heras 587" }]
+
+funcciones_var_dimensionadas.users_adress(client)
+
